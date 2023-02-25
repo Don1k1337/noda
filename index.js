26b10cce015a19b93f31cc22c8efd5fb82681322
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-02-25 17:12:10 +0600

    feat: mock data on post/get tested

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
 const app = express();
+const messages = []
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('Test');
+  res.render('index', { messages })
 });
 
-app.listen(3000, () => {
+app.post('/message/create', (req, res) => {
+  const body = req.body
+  const name = body.name
+  const message = body.message
+  if (name && message) {
+    messages.push({name, message})
+  }
+  res.redirect('/')
+})
+
+app.listen(1024, () => {
   console.log('App listening on port 3000!');
 });
