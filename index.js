50bff312e0c5a9880e01e6e6e61ab4bb8775631a
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-02-27 14:26:17 +0600

    feat: session cookies handler added

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
+const session = require("express-session");
 const app = express();
 const messages = []
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: process.env.ENV_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 
 app.get('/', (req, res) => {
-  res.render('index', { messages })
+  res.render('index', { messages, session: req.session })
 });
 
 app.post('/message/create', (req, res) => {
@@ -15,6 +24,8 @@ app.post('/message/create', (req, res) => {
   const message = body.message
   if (name && message) {
     messages.push({name, message})
+  } else {
+    req.session.error = 'Name or content cannot be empty!'
   }
   res.redirect('/')
 })
